af317b84642ab985d79a968277dfb925d548f6e0
inoKoichi <kouichi0_2012@yahoo.co.jp>
2018-04-16 03:12:40 +0900

    first commit

---
+++ src/main/java/com/yuka/artWorks/BlogTestApplication.java
@@ -0,0 +1,12 @@
+package com.yuka.artWorks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogTestApplication.class, args);
+	}
+}
